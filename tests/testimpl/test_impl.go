d9bd604baf973b03287c91b5fa1ebc98713fb3af
Ben Vaughan <160539772+ben-vaughan-nttd@users.noreply.github.com>
2026-08-27 11:06:33 -0500

    fix: consolidate Dependabot updates and Terratest 1.x (#20)

--- tests/testimpl/test_impl.go
+++ tests/testimpl/test_impl.go
@@ -38,9 +38,9 @@ func TestComposableMysqlServer(t *testing.T, ctx types.TestContext) {
 	}
 
 	t.Run("doesMysqlServerConfigurationExist", func(t *testing.T) {
-		resourceGroupName := terraform.Output(t, ctx.TerratestTerraformOptions(), "resource_group_name")
-		postgresName := terraform.Output(t, ctx.TerratestTerraformOptions(), "server_name")
-		postgresConfig := terraform.OutputMap(t, ctx.TerratestTerraformOptions(), "server_configuration")
+		resourceGroupName := terraform.OutputContext(t, context.Background(), ctx.TerratestTerraformOptions(), "resource_group_name")
+		postgresName := terraform.OutputContext(t, context.Background(), ctx.TerratestTerraformOptions(), "server_name")
+		postgresConfig := terraform.OutputMapContext(t, context.Background(), ctx.TerratestTerraformOptions(), "server_configuration")
 
 		for key := range postgresConfig {
 			mysqlConfig, err := armPostgresConfigClient.Get(context.Background(), resourceGroupName, postgresName, key, nil)
